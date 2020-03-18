5e7b228f513dbe175cc38cd5dc03339eed9ee281
anilcan <anillcan7@gmail.com>
2020-03-18 19:14:53 +0300

    Changed Solution of Hourglass

--- hourglass.py
+++ hourglass.py
@@ -1,12 +1,25 @@
+# Complete the hourglassSum function below.
 def hourglassSum(arr):
 
-    max1 = arr[0][0] + arr[0][1] + arr[0][2] 
-    max2 = arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]
-    maximum = max1 + max2
-
-    for i in range(len(arr)-2):
-        for j in range(len(arr)-2):
-            if (arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2] > maximum):
-                maximum = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
-
-    return maximum
+    # want to find the maximum hourglass sum
+    # minimum hourglass sum = -9 * 7 = -63
+    maxSum = -63
+    
+    for i in range(4):
+        for j in range(4):
+        
+            # sum of top 3 elements
+            top = sum(arr[i][j:j+3])
+            
+            # sum of the mid element
+            mid = arr[i+1][j+1]
+            
+            # sum of bottom 3 elements
+            bottom = sum(arr[i+2][j:j+3])
+            
+            hourglass = top + mid + bottom
+            
+            if hourglass > maxSum:
+                maxSum = hourglass
+                
+    return maxSum
